269c71002f7f40933a2d5c4b649483e0e7c311f4
Han Wei <tanhanwei90@gmail.com>
2022-04-09 03:13:42 +0800

    Created a custom execution contract for generic purposes

---
+++ Contracts/EGeneric.sol
@@ -0,0 +1,42 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.13;
+
+import "https://github.com/tanhanwei/Ollie-DAO/blob/main/Contracts/CustomExecution.sol";
+
+contract EGeneric is CustomExecutionContract {
+        //------------------------CUSTOM CODES------------------------------------------
+
+
+
+    // struct ExecutionParams {
+    //     bool[] BOOL;
+    //     uint256[] UINT;
+    //     string[] STRING;
+    //     address[] ADRESS;
+    // }
+    // struct Evidence {
+    //     string evidenceUri;
+    //     EvidenceType evidenceType;
+    // }
+
+    // enum EvidenceType {
+    //     TEXT,
+    //     NUMBER,
+    //     IMAGE,
+    //     VIDEO,
+    //     MIXED
+    // }
+    constructor() {
+        setExecutionName("Generic");
+    }
+    /*Parameter LUT for Generic:
+        UINT = [EVIDENCE_TYPE]
+        STRING = [EVIDENCE_URI]
+    */
+    function customExecution(address _dao, uint256 _id, ExecutionParams memory _executionParams) public override {
+        //CUSTOMIZE YOUR EXECUTION HERE
+        createEvidence(_dao, _id, EvidenceType(_executionParams.UINT[0]), _executionParams.STRING[0]);
+        completeExecution(_dao, _id);
+    }
+}
