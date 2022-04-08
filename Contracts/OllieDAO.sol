ab73b04b5b66bfb915da11d6b1bd5f20e3d8ebc8
Han Wei <tanhanwei90@gmail.com>
2022-04-09 00:50:00 +0800

    Added getter function

--- Contracts/OllieDAO.sol
+++ Contracts/OllieDAO.sol
@@ -2,7 +2,14 @@
 
 pragma solidity ^0.8.13;
 
+interface INft {
+    function owner() external view returns (address);
+    function name() external view returns (string calldata);
+    function ownerOf(uint256 tokenId) external view returns (address);
+}
+
 contract OllieDAO {
+
     address owner;
 
     struct Project {
@@ -11,8 +18,19 @@ contract OllieDAO {
         address owner;
     }
 
-    Project[] nft;
+    //can be accessed by nft[SC_ADDRESS]
+    mapping(address => Project) private nft;
 
-    mapping()
+    function getNftProjectOwner (address _nft) external view returns (address){
+        return INft(_nft).owner();
+    }
+
+    function getNftProjectName (address _nft) external view returns (string memory){
+        return INft(_nft).name();
+    }
+
+    function getNftTokenOwner(address _nft, uint256 _tokenId) external view returns (address){
+        return INft(_nft).ownerOf(_tokenId);
+    }
     
 }
