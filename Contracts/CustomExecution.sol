a0c4cbe93778d73e4e99149ada8f0a1be155f3ee
Han Wei <tanhanwei90@gmail.com>
2022-04-09 07:39:40 +0800

    Added address to string converter

--- Contracts/CustomExecution.sol
+++ Contracts/CustomExecution.sol
@@ -92,5 +92,23 @@ contract CustomExecutionContract {
     function customUserInterface() public virtual view returns (string memory) {
 
     }
+
+    // A utility to convert address to string
+    function toAsciiString(address x) public pure returns (string memory) {
+    bytes memory s = new bytes(40);
+    for (uint i = 0; i < 20; i++) {
+        bytes1 b = bytes1(uint8(uint(uint160(x)) / (2**(8*(19 - i)))));
+        bytes1 hi = bytes1(uint8(b) / 16);
+        bytes1 lo = bytes1(uint8(b) - 16 * uint8(hi));
+        s[2*i] = char(hi);
+        s[2*i+1] = char(lo);            
+    }
+    return string(s);
+}
+
+    function char(bytes1 b) public pure returns (bytes1 c) {
+        if (uint8(b) < 10) return bytes1(uint8(b) + 0x30);
+        else return bytes1(uint8(b) + 0x57);
+    }
   
 }
