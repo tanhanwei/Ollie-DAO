f2298f091d982e21fb6c0bc26be5048ff8660e2b
Han Wei <tanhanwei90@gmail.com>
2022-04-09 00:43:05 +0800

    Initial commit

---
+++ Contracts/OllieDAO.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.13;
+
+contract OllieDAO {
+    address owner;
+
+    struct Project {
+        uint32 network;
+        //address contractAddress;
+        address owner;
+    }
+
+    Project[] nft;
+
+    mapping()
+    
+}
