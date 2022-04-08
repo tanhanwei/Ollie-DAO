8dca1d1e64999daeeaf19e7623698a112fdfb1e3
Han Wei <tanhanwei90@gmail.com>
2022-04-09 01:18:48 +0800

    Created Custom DAO contract

--- Contracts/OllieDAO.sol
+++ Contracts/OllieDAO.sol
@@ -4,19 +4,47 @@ pragma solidity ^0.8.13;
 
 interface INft {
     function owner() external view returns (address);
-    //function name() external view returns (string);
+    function name() external view returns (string calldata);
     //function createProfile(DataTypes.CreateProfileData calldata vars) external;
+}
+
+interface IDao {
+    function getAdmins() external view returns (address[] calldata);
 }
 
 contract OllieDAO {
 
-    struct Dao {
-        address createdBy;
-        Membership membership;
-        address[] boardMembers;
+    address[] daos;
 
-        uint256 funds; //start with 0
+    function createDAO(address _project, bool _isERC721, address[] memory _admins) public payable{
+        require(msg.sender == getNftProjectOwner(_project), "Unauthorised");
+        CustomDao dao = new CustomDao(msg.sender, _project, _isERC721, _admins);
+        
+        daos.push(address(dao));
+
     }
+
+    function getNftProjectOwner (address _nft) private view returns (address){
+        return INft(_nft).owner();
+    }
+
+    function getAllDaos() public view returns (address[] memory) {
+        return daos;
+    }
+
+    function getDaoAdmins(address dao) public view returns (address[] memory){
+        return IDao(dao).getAdmins();
+    }
+
+}
+
+contract CustomDao {
+
+    address public creator;
+    address public project;
+    bool isERC721; //if false, then it's ERC20
+    address[] public admins;
+    Proposal[] public proposals;
 
     struct Proposal {
         string details; //ipfs json file
@@ -40,25 +68,19 @@ contract OllieDAO {
         REJECTED
     }
 
-    enum Membership {
-        erc721,
-        erc20
+    constructor(address _creator, address _project, bool _isERC721, address[] memory _admins){
+        creator = _creator;
+        project = _project;
+        isERC721 = _isERC721;
+        admins = _admins;
     }
 
-    //can be accessed by dao[PROJECT_ADDRESS]
-    mapping(address => Dao) private dao;
-
-    function createDAO(address _project, Membership _membership) public payable{
-        require(msg.sender == getNftProjectOwner(_project), "Unauthorised");
-        address[] memory boardMember;
-        boardMember[0] = msg.sender;
-        dao[_project] = Dao(msg.sender, _membership, boardMember, 0);
-
+    function getAdmins() public view returns (address[] memory){
+        return admins;
     }
 
-    function getNftProjectOwner (address _nft) private view returns (address){
-        return INft(_nft).owner();
+    function getProjectName() public view returns (string memory) {
+        return INft(project).name();
     }
 
-    
 }
