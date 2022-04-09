c334370a0ab6528e63012ad341004da2b97c5861
Han Wei <tanhanwei90@gmail.com>
2022-04-10 00:06:00 +0800

    Fixed weird array behaviour and browser is not "lying".

--- ollie-dao/src/Components/customDao.js
+++ ollie-dao/src/Components/customDao.js
@@ -1,6 +1,8 @@
 import { ethers } from "ethers";
 import { customDaoAbi } from "../abi/customDaoAbi";
 
+var proposals = Array();
+
 export const getDaoDetails = async (daoSc) => {
   let daoDetails = {
     name: "No Name",
@@ -25,9 +27,54 @@ export const getDaoDetails = async (daoSc) => {
 };
 
 export const getAllProposals = async (daoSc) => {
+  //convert proposals to objects
+
   const provider = new ethers.providers.Web3Provider(window.ethereum);
   const customDao = new ethers.Contract(daoSc, customDaoAbi, provider);
   const response = await customDao.getAllProposals();
 
-  console.log(response);
+  for (let i = 0; i < response.length; i++) {
+    var proposal = {
+      title: "",
+      details: "", //ipfs json file
+      status: 0,
+      executor: 0,
+      duration: 0,
+      evidence: "", //TODO link evidence from custom execution contract
+      execution: "", //smart contract that handles execution
+
+      votes: 0,
+      votedToken: [], //for ERC721
+      votedAccount: [], //for ERC20
+      totalSupply: 0,
+      voteType: 0,
+
+      //Challenge: challenge //TODO chain the challenge to proposal
+    };
+    proposal.details = response[i]["details"];
+    proposal.status = response[i]["status"];
+    proposal.executor = response[i]["executor"];
+    proposal.duration = Number(response[i]["duration"]["_hex"])
+      ? Number(response[i]["duration"]["_hex"])
+      : 0;
+    proposal.evidence = response[i]["evidence"];
+    proposal.execution = response[i]["execution"];
+    proposal.votes = Number(response[i]["hex"])
+      ? Number(response[i]["hex"])
+      : 0;
+    proposal.votedToken = response[i]["votedToken"];
+    proposal.votedAccount = response[i]["votedAccount"];
+    proposal.totalSupply = Number(response[i]["_hex"])
+      ? Number(response[i]["_hex"])
+      : 0;
+    proposal.voteType = response[i]["voteType"];
+    console.log(proposal);
+
+    proposals.push(proposal);
+    // console.log("push");
+    // console.log(proposals);
+  }
+
+  console.log(proposals);
+  //console.log(response[3]["details"]);
 };
