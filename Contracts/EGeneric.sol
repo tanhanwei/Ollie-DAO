675d9958dfae3cf85efd99b22d272aee288f20e5
Han Wei <tanhanwei90@gmail.com>
2022-04-09 08:09:36 +0800

    Updated template comments and Params LUT

--- Contracts/EGeneric.sol
+++ Contracts/EGeneric.sol
@@ -5,35 +5,32 @@ pragma solidity ^0.8.13;
 import "https://github.com/tanhanwei/Ollie-DAO/blob/main/Contracts/CustomExecution.sol";
 
 contract EGeneric is CustomExecutionContract {
-        //------------------------CUSTOM CODES------------------------------------------
 
-
-
-    // struct ExecutionParams {
-    //     bool[] BOOL;
-    //     uint256[] UINT;
-    //     string[] STRING;
-    //     address[] ADRESS;
-    // }
-    // struct Evidence {
-    //     string evidenceUri;
-    //     EvidenceType evidenceType;
-    // }
-
-    // enum EvidenceType {
-    //     TEXT,
-    //     NUMBER,
-    //     IMAGE,
-    //     VIDEO,
-    //     MIXED
-    // }
     constructor() {
         setExecutionName("Generic");
     }
-    /*Parameter LUT for Generic:
-        UINT = [EVIDENCE_TYPE]
-        STRING = [EVIDENCE_URI]
+    /*
+        struct Evidence {
+            string evidenceUri;
+            EvidenceType evidenceType;
+        }
+
+        EvidenceType:
+            0: TEXT
+            1: NUMBER
+            2: IMAGE
+            3: VIDEO
+            4: MIXED
+            5: URL
+
+        Parameter LUT for EXECUTION_NAME based on ExecutionParams struct:
+            BOOL = []
+            INT = []
+            UINT = [EVIDENCE_TYPE]
+            STRING = [EVIDENCE_URI]
+            ADDRESS = []
     */
+
     function customExecution(address _dao, uint256 _id, ExecutionParams memory _executionParams) public override {
         //CUSTOMIZE YOUR EXECUTION HERE
         createEvidence(_dao, _id, EvidenceType(_executionParams.UINT[0]), _executionParams.STRING[0]);
