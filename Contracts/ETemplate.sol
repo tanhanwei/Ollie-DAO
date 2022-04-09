8f73fd8ac26914f0048736ae43a3ca63dd31d56b
Han Wei <tanhanwei90@gmail.com>
2022-04-09 08:06:11 +0800

    Updated template comments

--- Contracts/ETemplate.sol
+++ Contracts/ETemplate.sol
@@ -4,39 +4,42 @@ pragma solidity ^0.8.13;
 
 import "https://github.com/tanhanwei/Ollie-DAO/blob/main/Contracts/CustomExecution.sol";
 
+//This is the template to create custom on-chain execution codes for Ollie DAO
 contract EExecutionName is CustomExecutionContract {
-        //------------------------CUSTOM CODES------------------------------------------
 
-
-
-    // struct ExecutionParams {
-    //     bool[] BOOL;
-    //     uint256[] UINT;
-    //     string[] STRING;
-    //     address[] ADRESS;
-    // }
-    // struct Evidence {
-    //     string evidenceUri;
-    //     EvidenceType evidenceType;
-    // }
-
-    // enum EvidenceType {
-    //     TEXT,
-    //     NUMBER,
-    //     IMAGE,
-    //     VIDEO,
-    //     MIXED
-    // }
     constructor() {
-        //setExecutionName("Generic");
+        //setExecutionName("EXECUTION_NAME");
     }
-    /*Parameter LUT for Generic:
-        UINT = []
-        STRING = []
+
+    /*
+        struct Evidence {
+            string evidenceUri;
+            EvidenceType evidenceType;
+        }
+
+        EvidenceType:
+            0: TEXT
+            1: NUMBER
+            2: IMAGE
+            3: VIDEO
+            4: MIXED
+            5: URL
+
+        Parameter LUT for EXECUTION_NAME based on ExecutionParams struct:
+            BOOL = []
+            INT = []
+            UINT = []
+            STRING = []
+            ADDRESS = []
     */
     function customExecution(address _dao, uint256 _id, ExecutionParams memory _executionParams) public override {
         //CUSTOMIZE YOUR EXECUTION HERE
+
+        //Create evidence based on Parameter LUT received
         //createEvidence(_dao, _id, EvidenceType(_executionParams.UINT[0]), _executionParams.STRING[0]);
-        //completeExecution(_dao, _id);
+        completeExecution(_dao, _id);
     }
+
+    //ADD YOUR CUSTOM CODE BELOW
+    
 }
