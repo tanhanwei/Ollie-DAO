e15603b9e6cddfdc1335a64c05e8bdfccf990f17
Han Wei <tanhanwei90@gmail.com>
2022-04-09 18:25:45 +0800

    added basic web3storage functions

---
+++ ollie-dao/src/Components/web3Storage.js
@@ -0,0 +1,65 @@
+import { Web3Storage } from "web3.storage";
+
+function getAccessToken() {
+  // If you're just testing, you can paste in a token
+  // and uncomment the following line:
+  return "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJkaWQ6ZXRocjoweEU5QUNhMkFDNDhjNTlENTA0MjU2YTM5OTE4ZTk2QzU4OTFERWJiMzYiLCJpc3MiOiJ3ZWIzLXN0b3JhZ2UiLCJpYXQiOjE2NDk0OTkwNzM0MjEsIm5hbWUiOiJPbGxpZSBEYW8ifQ.0XywBxSKKTcJWNl9IgXl1oX5ilFEOmfHzAH-xocSbiY";
+
+  // In a real app, it's better to read an access token from an
+  // environement variable or other configuration that's kept outside of
+  // your code base. For this to work, you need to set the
+  // WEB3STORAGE_TOKEN environment variable before you run your code.
+  //return process.env.WEB3STORAGE_TOKEN
+}
+
+function makeStorageClient() {
+  return new Web3Storage({ token: getAccessToken() });
+}
+
+function makeFileObjects() {
+  // You can create File objects from a Blob of binary data
+  // see: https://developer.mozilla.org/en-US/docs/Web/API/Blob
+  // Here we're just storing a JSON object, but you can store images,
+  // audio, or whatever you want!
+  const obj = { hello: "world" };
+  const blob = new Blob([JSON.stringify(obj)], { type: "application/json" });
+
+  const files = [
+    new File(["contents-of-file-1"], "plain-utf8.txt"),
+    new File([blob], "hello.json"),
+  ];
+  return files;
+}
+
+async function storeFiles(files) {
+  const client = makeStorageClient();
+  const cid = await client.put(files);
+  console.log("stored files with cid:", cid);
+  return cid;
+}
+
+async function retrieve(cid) {
+  const client = makeStorageClient();
+  const res = await client.get(cid);
+  console.log(`Got a response! [${res.status}] ${res.statusText}`);
+  if (!res.ok) {
+    throw new Error(`failed to get ${cid}`);
+  }
+
+  // request succeeded! do something with the response object here...
+}
+
+async function retrieveFiles(cid) {
+  const client = makeStorageClient();
+  const res = await client.get(cid);
+  console.log(`Got a response! [${res.status}] ${res.statusText}`);
+  if (!res.ok) {
+    throw new Error(`failed to get ${cid} - [${res.status}] ${res.statusText}`);
+  }
+
+  // unpack File objects from the response
+  const files = await res.files();
+  for (const file of files) {
+    console.log(`${file.cid} -- ${file.name} -- ${file.size}`);
+  }
+}
