dd193f32bde26567196ace3ade7e95d410b326d5
Han Wei <tanhanwei90@gmail.com>
2022-04-10 22:08:57 +0800

    Dropdown not working

--- ollie-dao/src/routes/propose.jsx
+++ ollie-dao/src/routes/propose.jsx
@@ -1,18 +1,78 @@
-import { Box, Button, TextField } from "@mui/material";
-import React, { useState } from "react";
+import {
+  Box,
+  Button,
+  FormControl,
+  InputLabel,
+  MenuItem,
+  Select,
+  TextField,
+} from "@mui/material";
+import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { createProposal } from "../Components/proposal";
 import { TEMPExecutionSc } from "../network";
+import { OllieDAOSc } from "../network";
+import { getVerifiedExecutionContracts } from "../Components/ollieDao";
+import { getExecutionName } from "../Components/execute";
 
 const Propose = () => {
   const [title, setTitle] = useState();
   const [details, setDetails] = useState();
   const { daoAddress } = useParams();
+  const [executors, setExecutors] = useState([]);
+  const [selectedContract, setSelectedContract] = useState();
+
+  useEffect(() => {
+    async function inner() {
+      let allExecution = [];
+      const response = await getVerifiedExecutionContracts(OllieDAOSc);
+      console.log(response);
+      for (let i = 0; i < response.length; i++) {
+        let exeName = await getExecutionName(response[i]);
+        let execution = {
+          contract: response[i],
+          name: exeName,
+        };
+        allExecution.push(execution);
+      }
+      //console.log(allExecution);
+      setExecutors(allExecution);
+
+      //console.log(executors);
+    }
+    inner();
+  }, []);
+  useEffect(() => console.log(executors), [executors]);
+
+  //   useEffect(() => {
+  //     async function fetchData() {
+  //       let allExecution = [];
+  //       const response = await getVerifiedExecutionContracts(OllieDAOSc);
+  //       console.log(response);
+  //       for (let i = 0; i < response.length; i++) {
+  //         let exeName = await getExecutionName(response[i]);
+  //         let execution = {
+  //           contract: response[i],
+  //           name: exeName,
+  //         };
+  //         allExecution.push(execution);
+  //       }
+  //       console.log(allExecution);
+  //       setExecutors(allExecution);
+
+  //       console.log(executors);
+  //     }
+  //     fetchData();
+  //   }, []);
 
   const handleChangeTitle = (event) => {
     setTitle(event.target.value);
   };
   const handleChangeDetails = (event) => {
+    setDetails(event.target.value);
+  };
+
+  const handleDropDownChange = (event) => {
     setDetails(event.target.value);
   };
 
@@ -49,6 +109,24 @@ const Propose = () => {
             onChange={handleChangeDetails}
           />
           <Box>
+            <FormControl fullWidth>
+              <InputLabel id="demo-simple-select-label">
+                Executor Smart Contract
+              </InputLabel>
+              <Select
+                labelId="demo-simple-select-label"
+                id="demo-simple-select"
+                value={0}
+                label="Age"
+                onChange={handleDropDownChange}
+              >
+                {executors.map((executor) => {
+                  <MenuItem value={executor.contract}>
+                    {executor.name}
+                  </MenuItem>;
+                })}
+              </Select>
+            </FormControl>
             <Button
               onClick={handleButtonClick}
               variant="contained"
